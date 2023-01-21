0634ca6e2e40150bb2b4a8a8a60b2a178f9d5ad7
Adam Toth <adam_toth4@epam.com>
2023-01-22 00:02:45 +0100

    121. Best Time to Buy and Sell Stock

---
+++ LeetCode/121-BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode;
+
+public static class BestTimeToBuyAndSellStock
+{
+    public static int MaxProfit(int[] prices)
+    {
+        if (prices.Length == 0)
+            return 0;
+
+        int minPriceSoFar = prices[0];
+        int maxProfitSoFar = 0;
+
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (maxProfitSoFar < prices[i] - minPriceSoFar)
+            {
+                maxProfitSoFar = prices[i] - minPriceSoFar;
+            }
+            if (minPriceSoFar > prices[i])
+            {
+                minPriceSoFar = prices[i];
+            }
+        }
+
+        return maxProfitSoFar;
+    }
+}
---
+++ LeetCodeTests/121-BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,38 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests;
+
+public class BestTimeToBuyAndSellStockTests
+{
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+
+        int expected = 5;
+
+        // Act
+        var result = BestTimeToBuyAndSellStock.MaxProfit(prices);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+
+    [Test]
+    public void Test2()
+    {
+        // Arrange
+        int[] prices = { 7, 6, 4, 3, 1 };
+
+        int expected = 0;
+
+        // Act
+        var result = BestTimeToBuyAndSellStock.MaxProfit(prices);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
