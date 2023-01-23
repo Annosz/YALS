7315388d832fcb54e34c89b996709b6f6ce478df
Adam Toth <adam_toth4@epam.com>
2023-01-23 08:32:15 +0100

    122. Best Time to Buy and Sell Stock II

---
+++ LeetCode/122-BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode;
+
+public static class BestTimeToBuyAndSellStockII
+{
+    public static int MaxProfit(int[] prices)
+    {
+        if (prices.Length == 0)
+            return 0;
+
+        int minPriceSinceLastSell = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.Length - 1; i++)
+        {
+            if (prices[i] > prices[i + 1] && minPriceSinceLastSell < prices[i])
+            {
+                maxProfit += prices[i] - minPriceSinceLastSell;
+                minPriceSinceLastSell = prices[i];
+            }
+            if (minPriceSinceLastSell > prices[i])
+            {
+                minPriceSinceLastSell = prices[i];
+            }
+        }
+
+        // Chance to sell at last day
+        if (minPriceSinceLastSell < prices[^1])
+        {
+            maxProfit += prices[^1] - minPriceSinceLastSell;
+        }
+
+        return maxProfit;
+    }
+}
---
+++ LeetCodeTests/122-BestTimeToBuyAndSellStockIITests.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests;
+
+public class BestTimeToBuyAndSellStockIITests
+{
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+
+        int expected = 7;
+
+        // Act
+        var result = BestTimeToBuyAndSellStockII.MaxProfit(prices);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        // Arrange
+        int[] prices = { 1, 2, 3, 4, 5 };
+
+        int expected = 4;
+
+        // Act
+        var result = BestTimeToBuyAndSellStockII.MaxProfit(prices);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        // Arrange
+        int[] prices = { 7, 6, 4, 3, 1 };
+
+        int expected = 0;
+
+        // Act
+        var result = BestTimeToBuyAndSellStockII.MaxProfit(prices);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
