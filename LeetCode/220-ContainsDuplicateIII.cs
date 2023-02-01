73e1416be1f98e84efa26e2d35c8471f5e361457
Adam Toth <adam_toth4@epam.com>
2023-02-01 16:15:39 +0100

    220. Contains Duplicate III

---
+++ LeetCode/220-ContainsDuplicateIII.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode;
+
+public static class ContainsDuplicateIII
+{
+    public static bool ContainsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff)
+    {
+        SortedSet<int> numbersWithinIndexDiff = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (numbersWithinIndexDiff.GetViewBetween(nums[i] - valueDiff, nums[i] + valueDiff).Any())
+                return true;
+
+            numbersWithinIndexDiff.Add(nums[i]);
+
+            // We can safely remove the element, if it would be a duplicate, we would be already returning true
+            if (i >= indexDiff)
+                numbersWithinIndexDiff.Remove(nums[i - indexDiff]);
+        }
+
+        return false;
+    }
+}
---
+++ LeetCodeTests/220-ContainsDuplicateIIITests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests;
+
+public class ContainsDuplicateIIITests
+{
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        int[] nums = { 1, 2, 3, 1 };
+        int indexDiff = 3;
+        int valueDiff = 0;
+
+        bool expected = true;
+
+        // Act
+        var result = ContainsDuplicateIII.ContainsNearbyAlmostDuplicate(nums, indexDiff, valueDiff);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        // Arrange
+        int[] nums = { 1, 5, 9, 1, 5, 9 };
+        int indexDiff = 2;
+        int valueDiff = 3;
+
+        bool expected = false;
+
+        // Act
+        var result = ContainsDuplicateIII.ContainsNearbyAlmostDuplicate(nums, indexDiff, valueDiff);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
