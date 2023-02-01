ee50e7c8a75193e0bb43ce97f66da0a3ece00d67
Adam Toth <adam_toth4@epam.com>
2023-02-01 15:26:38 +0100

    217. Contains Duplicate

---
+++ LeetCode/217-ContainsDuplicate.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode;
+
+public static class ContainsDuplicate
+{
+    public static bool ContainsDuplicateX(int[] nums)
+    {
+        HashSet<int> elements = new HashSet<int>();
+        foreach (int num in nums)
+        {
+            if (elements.Contains(num))
+            {
+                return true;
+            }
+
+            elements.Add(num);
+        }
+        return false;
+    }
+}
