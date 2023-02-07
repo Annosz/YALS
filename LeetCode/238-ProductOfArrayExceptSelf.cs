bb727ad50cf3f9d59c9c9cd4c3111a65a1862120
Adam Toth <adam_toth4@epam.com>
2023-02-07 13:30:34 +0100

    238. Product of Array Except Self

---
+++ LeetCode/238-ProductOfArrayExceptSelf.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode;
+
+public static class ProductOfArrayExceptSelf
+{
+    public static int[] ProductExceptSelf(int[] nums)
+    {
+        int[] leftProducts = GenerateLeftProducts(nums);
+        int[] rightProducts = GenerateRightProducts(nums);
+
+        return nums.Select((_, i) => leftProducts[i] * rightProducts[i]).ToArray();
+    }
+
+    private static int[] GenerateLeftProducts(int[] nums)
+    {
+        List<int> leftProducts = new() { 1 };
+        for (int i = 1; i < nums.Length; i++)
+        {
+            leftProducts.Add(leftProducts[i - 1] * nums[i - 1]);
+        }
+
+        return leftProducts.ToArray();
+    }
+
+    private static int[] GenerateRightProducts(int[] nums)
+    {
+        List<int> leftProducts = new() { 1 };
+        for (int i = 1; i < nums.Length; i++)
+        {
+            leftProducts.Insert(0, leftProducts[^i] * nums[^i]);
+        }
+        return leftProducts.ToArray();
+    }
+}
---
+++ LeetCodeTests/238-ProductOfArrayExceptSelfTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode;
+
+public class ProductOfArrayExceptSelfTests
+{
+    [Test]
+    public void Test1()
+    {
+        // Arrange
+        int[] nums = { 1, 2, 3, 4 };
+
+        int[] expected = { 24, 12, 8, 6 };
+
+        // Act
+        var result = ProductOfArrayExceptSelf.ProductExceptSelf(nums);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        // Arrange
+        int[] nums = { -1, 1, 0, -3, 3 };
+
+        int[] expected = { 0, 0, 9, 0, 0 };
+
+        // Act
+        var result = ProductOfArrayExceptSelf.ProductExceptSelf(nums);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
